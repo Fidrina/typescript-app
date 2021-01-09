bad4ad52d23ceac699abf4f84b8e797864c1999b
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-01-09 17:11:12 -0300

    routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -4,16 +4,15 @@ import { createUserController } from './useCases/CreateUser';
 const router = Router();
 
 router.post('/users', (request, response) => {
-    return createUserController.handle(request, response);
+  return createUserController.handle(request, response);
 });
-
 router.get('/users', (request, response) => {
-    return response.status(200).json([
-        { id: 1, name: "Ana" },
-        { id: 2, name: "Bia" },
-        { id: 3, name: "Lucia" },
-        { id: 4, name: "Carol" }
-    ]);
-})
+  return response.status(200).json([
+    { id: 1, name: "Ana" },
+    { id: 2, name: "Bia" },
+    { id: 3, name: "Lucia" },
+    { id: 4, name: "Carol" }
+  ]);
+});
 
 export { router };
