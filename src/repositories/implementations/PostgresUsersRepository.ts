6e1a64dd4ebc0a8142acf33145cded3bca7f54db
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-01-09 17:14:35 -0300

    PostgresUsersRepository.ts

--- src/repositories/implementations/PostgresUsersRepository.ts
+++ src/repositories/implementations/PostgresUsersRepository.ts
@@ -1,15 +1,16 @@
-import { User } from "../../entities/User";
 import { IUsersRepository } from "../IUsersRepository";
+import { User } from "../../entities/User";
 
 export class PostgresUsersRepository implements IUsersRepository {
-    private users: User[] = [];
-    async findByEmail(email: string): Promise<User> {
-        const user = this.users.find(user => user.email === email);
+  private users: User[] = [];
 
-        return user;
-    }
-    async save(user: User): Promise<void> {
-        this.users.push(user);
-    }
+  async findByEmail(email: string): Promise<User> {
+    const user = this.users.find(user => user.email === email);
 
+    return user;
+  }
+
+  async save(user: User): Promise<void> {
+    this.users.push(user);
+  }
 }
